8b383d499242fe6a1452c02fbd87b263e519af9d
Anatolii Ganchev <anatolii.ganchev@scayla.net>
2021-07-18 23:40:30 +0200

    add dependencies and main app class

---
+++ src/main/java/dice/avaloq/AvaloqDiceTaskApp.java
@@ -0,0 +1,11 @@
+package dice.avaloq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvaloqDiceTaskApp {
+    public static void main(String[] args) {
+        SpringApplication.run(AvaloqDiceTaskApp.class, args);
+    }
+}
